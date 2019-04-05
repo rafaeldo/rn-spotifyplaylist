070e115b4ce9c26cc177b468db74abfc21e21fd3
Rafael de Oliveira <rafaoliverdev@gmail.com>
2019-04-05 14:37:05 -0300

    add: podcast Duck.

---
+++ src/store/ducks/player.js
@@ -0,0 +1,30 @@
+import { createReducer, createActions } from 'reduxsauce';
+import Immutable from 'seamless-immutable';
+
+/**
+ * ACTION TYPES and CREATORS
+ */
+const { Types, Creators } = createActions({
+  setPodcastReq: ['podcast', 'episodeId'],
+  setPodcastSuc: ['podcast'],
+});
+export const PodcastsTypes = Types;
+export default Creators;
+
+/**
+ * INITIAL STATE
+ */
+export const INITIAL_STATE = Immutable({
+  podcast: null,
+  current: null,
+});
+// The Immutable function has some methods,
+// like 'merge', that is used below, inside
+// the Reducer.
+
+/**
+ * REDUCER
+ */
+export const reducer = createReducer(INITIAL_STATE, {
+  [Types.SET_PODCAST_SUC]: (state, { podcast }) => state.merge({ podcast, current: podcast.tracks[0].id }),
+});
