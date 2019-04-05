857c40ea3f45a7c960f18bd0317e3f73d315bdbc
Rafael de Oliveira <rafaoliverdev@gmail.com>
2019-04-05 13:27:07 -0300

    update: Podcast page layout.

--- src/pages/Podcast/index.js
+++ src/pages/Podcast/index.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
 
-import { View } from 'react-native';
-
-// import { Container } from './styles';
+import {
+  Container,
+  EpisodeList,
+  PodcastDetails,
+  Background,
+  Cover,
+  PodcastTitle,
+  PlayButton,
+  PlayButtonText,
+  Episode,
+  Author,
+  Title,
+} from './styles';
 
 export default class Podcast extends Component {
+  componentDidMount() {}
+
   render() {
-    return <View />;
+    const { navigation } = this.props;
+    const podcast = navigation.getParam('podcast');
+
+    return (
+      <Container>
+        <EpisodeList
+          ListHeaderComponent={() => (
+            <PodcastDetails>
+              <Background source={{ uri: podcast.cover }} blurRadius={5} />
+              <Cover source={{ uri: podcast.cover }} />
+
+              <PodcastTitle>{podcast.title}</PodcastTitle>
+
+              <PlayButton onPress={() => {}}>
+                <PlayButtonText>REPRODUZIR</PlayButtonText>
+              </PlayButton>
+            </PodcastDetails>
+          )}
+          data={podcast.tracks}
+          keyExtractor={episode => String(episode.id)}
+          renderItem={({ item: episode }) => (
+            <Episode>
+              <Title>{episode.title}</Title>
+              <Author>{episode.artist}</Author>
+            </Episode>
+          )}
+        />
+      </Container>
+    );
   }
 }
