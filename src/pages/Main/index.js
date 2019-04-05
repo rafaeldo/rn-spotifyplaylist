e5961689614c8fbb80a86dc2d1004db43b9dc770
Rafael de Oliveira <rafaoliverdev@gmail.com>
2019-04-05 11:37:15 -0300

    update: Main page now with a styled FlatList

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
-import { Container } from './styles';
+import {
+  Container, PodcastList, PageTitle, Podcast, Cover, Info, Title, Count,
+} from './styles';
+
+import podcasts from '~/utils/podcastsArr';
 
 export default class Main extends Component {
+  componentDidMount() {}
+
   render() {
-    return <Container />;
+    return (
+      <Container>
+        <PodcastList
+          ListHeaderComponent={() => <PageTitle>Podcasts</PageTitle>}
+          data={podcasts}
+          keyExtractor={podcast => String(podcast.id)}
+          renderItem={({ item: podcast }) => (
+            <Podcast onPress={() => {}}>
+              <Cover source={{ uri: podcast.cover }} />
+              <Info>
+                <Title>{podcast.title}</Title>
+                <Count>{`${podcast.tracks.length} episódios`}</Count>
+              </Info>
+            </Podcast>
+          )}
+        />
+      </Container>
+    );
   }
 }
